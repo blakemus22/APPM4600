dc8da30c9b58892334334684c6b548ffcabbf3df
Blake Musfeldt <blake.musfeldt@colorado.edu>
2024-11-17 19:57:28 -0700

    made another good plot

--- Labs/project.py
+++ Labs/project.py
@@ -6,8 +6,24 @@ x = np.linspace(-1,1,500)
 eee = np.e**x
 guess = x+1.2
 
+# plt.plot(x,eee, color = "indianred", label = "$f(x) = e^x$")
+# plt.plot(x, guess, color = "slateblue", label = "Minimax guess, $p(x) = x + 1.2$")
+# plt.legend()
+# plt.title("Guess for minimax approximation")
+# plt.show()
+
+a1 = (np.e - 1/np.e)/2
+a0 = (np.e - a1*np.log(a1))/2
+p = a1 * x + a0
+
+error = eee - p
+plt.subplot(1,2,1)
 plt.plot(x,eee, color = "indianred", label = "$f(x) = e^x$")
-plt.plot(x, guess, color = "slateblue", label = "Minimax guess, $f(x) = x + 1.2$")
+plt.plot(x, p, color = "slateblue", label = "p(x):  Minimax approximation")
 plt.legend()
-plt.title("Guess for minimax approximation")
+plt.title("Calculated minimax approximation")
+
+plt.subplot(1,2,2)
+plt.plot(x,abs(error), color = "slateblue")
+plt.title("Absolute error of the approximation")
 plt.show()
